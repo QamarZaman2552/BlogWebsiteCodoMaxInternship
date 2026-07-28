187d8b260e0fe4f2b433bf91b1c664098bcdc382
QAMAR ZAMAN BALOCH <qamarbaloch2023@gmail.com>
2026-07-28 10:51:01 +0500

    Day 2 - Add HTML pages (Home, Add Blog, nav, footer)

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Blog Management System is running.');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
